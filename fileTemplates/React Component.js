ae7cc473d4da1a97d63a3e94e6e04f1d8accd9b6
Michael Schinis <mschinis@gmail.com>
2017-08-26 17:32:38 +0100

    WS-2017.1.3 <mschinis@michaels-mbp Create baseRefactoring.xml, _@user_Material Theme - Default.icls, ide.general.xml, project.default.xml, git.xml, other.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, plugin_ui.xml, notifications.xml, github_settings.xml, ReactJS.xml, React Style.js, Default.xml, JavaScript.xml, laf.xml, Mac OS X 10_5_ copy.xml, Handlebars.xml, editor.codeinsight.xml, keymap.xml, React Component.js, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/React Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ${NAME} extends React.Component {
+    static propTypes = {
+    
+    }
+
+    render() {
+        return (
+        
+        );
+    }
+};
